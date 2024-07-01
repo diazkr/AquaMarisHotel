7a112e30968e075bcf2ee04c53f6b9a032fe81b9
Araceli Hernandez Gutierrez <aracelihg@MacBook-Air-de-Araceli.local>
2024-06-30 19:18:02 -0600

    incorporando icons

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Modal from 'react-modal';
 import { Habitacion } from '@/interfaces/HabitacionInterface';
+import {FaWifi, FaTv, FaWater, FaSnowflake,FaFire,FaLock,FaParking,FaCoffee,FaIceCream,FaHotTub,} from "react-icons/fa";
 
 const Page = ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -17,6 +17,62 @@ const Page = ({ params }: { params: { id: string } }) => {
 
   const closeModal = () => {
     setModalIsOpen(false);
+  };
+
+  const renderIcon = (servicio: string) => {
+    switch (servicio) {
+      case "wifi":
+        return <FaWifi key={servicio} />;
+      case "television":
+        return <FaTv key={servicio} />;
+      case "seaView":
+        return <FaWater key={servicio} />;
+      case "airConditioning":
+        return <FaSnowflake key={servicio} />;
+      case "heater":
+        return <FaFire key={servicio} />;
+      case "safeBox":
+        return <FaLock key={servicio} />;
+      case "parking":
+        return <FaParking key={servicio} />;
+      case "fridge":
+        return <FaCoffee key={servicio} />;
+      case "breakfast":
+        return <FaIceCream key={servicio} />;
+      case "jacuzzi":
+        return <FaHotTub key={servicio} />;
+      default:
+        return null;
+    }
+
+  };
+
+  const renderTextEspanol = (servicio: string) => {
+    switch (servicio) {
+      case "wifi":
+        return "WiFi";
+      case "television":
+        return "Televisión";
+      case "seaView":
+        return "Vista al mar";
+      case "airConditioning":
+        return "Aire acondicionado";
+      case "heater":
+        return "Calefacción";
+      case "safeBox":
+        return "Caja fuerte";
+      case "parking":
+        return "Estacionamiento";
+      case "fridge":
+        return "Refrigerador";
+      case "breakfast":
+        return "Desayuno";
+      case "jacuzzi":
+        return "Jacuzzi";
+      default:
+        return null;
+    }
+
   };
 
   useEffect(() => {
@@ -54,7 +110,7 @@ const Page = ({ params }: { params: { id: string } }) => {
               <Image key={index} src={image} alt={"view"} width={400} height={300} className="rounded-lg w-full h-full" />
             ))}
             <div className="relative">
-              <Image src={room.images[4]} alt="View 5" width={400} height={300} className="rounded-lg w-full h-auto" />
+              <Image src={room.images[4]} alt="View 5" width={400} height={300} className="rounded-lg w-full h-full" />
               <button
                 className="absolute inset-0 bg-black bg-opacity-50 text-white flex justify-center items-center rounded-lg"
                 onClick={openModal}
@@ -82,11 +138,23 @@ const Page = ({ params }: { params: { id: string } }) => {
                   <span className="text-lg font-semibold text-gray-600">Número de habitación</span>
                   <span className="ml-2 text-gray-500">{room.roomNumber}</span>
                 </div>
-                <div className="flex items-center">
+                <div className="flex">
                   <span className="text-lg font-semibold text-gray-600">Servicios</span>
-                  <span className="ml-2 text-gray-500">{room.services.join(', ')}</span>
+                  <span className="ml-2 text-gray-500">{room.services.map((servicio) => (renderTextEspanol(servicio))).join(', ')}</span>
                 </div>
-              </div>
+                <div className="flex items-center">
+                  <div className="flex space-x-2 text-2xl text-[#17858A] my-3">
+                        {room.services.map((servicio) => (
+                      <div
+                            key={servicio}
+                            className="flex items-center justify-center w-12 h-12 rounded-full border border-gray-300 hover:bg-[#d9eeec] hover:scale-95 transition-transform duration-200"
+                          >
+                            {renderIcon(servicio)}
+                          </div>
+                          ))}
+                      </div>
+                  </div>
+                </div>
             </div>
             <button className="w-full bg-teal-600 text-white py-2 px-4 rounded-lg hover:bg-teal-500" disabled>
               Calendario
