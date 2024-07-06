23a405a030cc4c36220eac51ce43a77b36f5470d
kdiazc1 <kdiazc@unal.edu.co>
2024-07-06 16:15:04 -0500

    feat(): hero de experincias

--- src/app/experiencias/page.tsx
+++ src/app/experiencias/page.tsx
@@ -1,13 +1,21 @@
 import MapComponent from '@/componentes/mapa/MapComponent';
 import React from 'react';
 import dynamic from "next/dynamic"
+import ImageExp from '@/componentes/mapa/ImageExperience';
 
 const DynamicMap = dynamic(() => import('../../componentes/mapa/MapComponent'),{ssr:false})
 
 const HotelPage = () => {
   return (
-    <div className='my-24 w-[60%]' style={{height: '500px' }}>
+    <div>
+
+      <ImageExp/>
+
+
+    
+    <div className='my-24' style={{height: '500px' }}>
       <DynamicMap />
+    </div>
     </div>
   );
 };
